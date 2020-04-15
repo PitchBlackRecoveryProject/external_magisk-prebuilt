79d93227fe6786efbe777a3c8cd68b052e332304
mauronofrio <poronofrio@gmail.com>
2020-04-15 19:16:17 +0000

    Switch from eng to optional for Android 10
    Change-Id: Ic5d3a0b9b3beb2b605e6c481a4c416fa3b6d1659

--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -10,7 +10,11 @@ endif
 include $(CLEAR_VARS)
 LOCAL_MODULE := magiskboot
 LOCAL_MODULE_STEM := magiskboot
-LOCAL_MODULE_TAGS := eng
+    ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 29; echo $$?),0)
+        LOCAL_MODULE_TAGS := optional
+    else
+        LOCAL_MODULE_TAGS := eng
+    endif
 LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_SRC_FILES := $(MAGISKBOOT_TARGET)
